09bba3d1250e8dbff9975d0ca47b344be20b0fd1
Aalan Miranda <aalanamiranda@gmail.com>
2020-08-13 12:38:37 -0400

    API #1 Improvements

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-
+//console.log('connected');
 
 const baseURL = 'https://api.spacexdata.com/v4/launches/latest';
 
-const searchForm = document.querySelector('form');
+const search = document.getElementById('space');
 const spaceShips = document.querySelector('ul');
 
-searchForm.addEventListener('submit', fetchSpace);
+search.addEventListener('click', fetchSpace);
 
-function fetchSpace() {
-  event.preventDefault();
-  // console.log("form was submitted");
+function fetchSpace(e) {
+  event.preventDefault(e);
+  //console.log("form was submitted");
 
   /**
-   * fetch starts the proces of fetching a resource from a 
+   * fetch starts the process of fetching a resource from a 
    * network and that fetch returns a promise which is fulfilled once the response is available
    */
   
   //fetch starts the process of fetching a resource from a network and that fetch returns a promise which is fulfilled once the response is available
   fetch(baseURL)
-    .then(result => { 
-      return result.json();
+    .then( data => { 
+      
+      return data.json();
     })
-    .then(data => {
-      console.log(data);
-      displayRockets(data);
+    .then(json => {
+      //console.log(json);
+      display(json);
     })
     .catch(err => console.log(err));
 }
 
-function displayRockets(passedData){
-    console.log(passedData);
-    passedData.forEach(element => {
-        let rocketLI = document.createElement('li');
-        let picContainer = document.createElement('img');
-        rocketLI.innerText = element.name;
-        if(element.flickr_images.length > 0){
-        picContainer.src = element.flickr_images[0];
-        } else {
-            picContainer.alt = 'no image available';
-        }
-        
-        spaceShips.appendChild(rocketLI);
-        //let pic = document.createElement('img');
-        rocketLI.appendChild(picContainer);
-    })
+function display(passedData){
+  //element creation
+    let spaceContent = document.getElementById('content-container');
+    let contentHeader = document.createElement('h2');
+    let listParagraph = document.createElement('p');
+    let contentInformation = document.createElement('div');
+    let informationHeader = document.createElement('h2');
+    let informationList = document.createElement('ul');
+    let youtubeListItem = document.createElement('li');
+
+    //element assignments
+    contentHeader.innerText = 'More SpaceX Stuff!';
+    listParagraph.innerText = passedData.details;
+    informationHeader.innerText = 'More Information';
+    //youtubeListItem.src = passedData.links.webcast;
+    youtubeListItem.innerText = passedData.links.webcast;
+
+    //DOM Tree Navigation and relationships
+    spaceContent.appendChild(contentHeader);
+    spaceContent.appendChild(contentInformation);
+    contentHeader.appendChild(listParagraph);
+    contentInformation.appendChild(informationHeader);
+    contentInformation.appendChild(informationList);
+    informationList.appendChild(youtubeListItem);
+
+    
+    
 }
